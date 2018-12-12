24f4ab19839a3c1bfbf8b94873e0f85dd3086b79
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2018-12-12 17:09:42 +0100

    Implémentation des commandes de base & mise à jour du gitignore

--- kvstore/src/main/java/org/example/kvstore/StoreImpl.java
+++ kvstore/src/main/java/org/example/kvstore/StoreImpl.java
@@ -1,32 +1,68 @@
 package org.example.kvstore;
 
 import org.example.kvstore.cmd.CommandFactory;
+import org.example.kvstore.distribution.ConsistentHash;
 import org.example.kvstore.distribution.Strategy;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Map;
 
-public class StoreImpl<K,V> implements Store<K,V>{
+public class StoreImpl<K,V> extends ReceiverAdapter implements Store<K,V> {
 
+    /**
+     * Nom ?
+     */
     private String name;
-    private Strategy strategy;
-    private Map<K,V> data;
-    private CommandFactory<K,V> factory;
 
+    /**
+     * Champs définissant le "plan" de stockage
+     */
+    private Strategy strategy;
+
+    /**
+     * Données du noeud
+     */
+    private Map<K, V> data;
+
+    /**
+     * ?
+     */
+    private CommandFactory<K, V> factory;
+
+    /**
+     * @param name
+     *              nom ?
+     */
     public StoreImpl(String name) {
         this.name = name;
     }
 
-    public void init() throws Exception{
+    /**
+     * Initialise le noeud
+     */
+    public void init() throws Exception {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public void viewAccepted(View view) {
+        this.strategy = new ConsistentHash(view);
     }
 
     @Override
     public V get(K k) {
-      return null;
+        return data.get(k);
     }
 
     @Override
     public V put(K k, V v) {
-      return null;
+        V old = data.get(k);
+        data.put(k, v);
+        return old;
     }
 
     @Override
--- kvstore/src/main/java/org/example/kvstore/distribution/ConsistentHash.java
+++ kvstore/src/main/java/org/example/kvstore/distribution/ConsistentHash.java
@@ -3,21 +3,45 @@ package org.example.kvstore.distribution;
 import org.jgroups.Address;
 import org.jgroups.View;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 
-public class ConsistentHash implements Strategy{
+public class ConsistentHash implements Strategy {
 
+    /**
+     * Representation of the cluster
+     */
     private TreeSet<Integer> ring;
-    private Map<Integer,Address> addresses;
 
-    public ConsistentHash(View view){
+    /**
+     * Map Integer to addresses (node's id)
+     */
+    private Map<Integer, Address> addresses;
+
+    /**
+     * Construit le ring en fonction des noeuds ups
+     * @param view
+     *              la vue actuelle
+     */
+    public ConsistentHash(View view) {
+        this.ring = new TreeSet<>();
+        this.addresses = new HashMap<>();
+
+        List<Address> memberList = view.getMembers();
+        int size = memberList.size();
+        int id;
+        for (int i=0; i<size; i++) {
+            id = i* Integer.MAX_VALUE/size;
+            ring.add(id);
+            addresses.put(id, memberList.get(i));
+        }
     }
 
     @Override
-    public Address lookup(Object key){
-      return null;
+    public Address lookup(Object key) {
+        Integer id = ring.floor(key.hashCode());
+        if (id != null) {
+            return addresses.get(id);
+        }
+        return null;
     }
-
 }
