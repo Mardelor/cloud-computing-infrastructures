0da30589a0609bf5c4c0ebd471d84aeb87850ba4
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-01-09 18:02:05 +0100

    Implémentation élection naïve

--- zk/election.py
+++ zk/election.py
@@ -6,10 +6,11 @@ from kazoo.exceptions import KazooException
 
 class Election:
 
-    def __init__(self, zk, path, func,args):
+    def __init__(self, zk, path, func, args):
         self.election_path = path
         self.zk = zk
         self.is_leader = False
+        self.path = None
         if not (inspect.isfunction(func)) and not(inspect.ismethod(func)):
             logging.debug("not a function "+str(func))
             raise SystemError
@@ -18,7 +19,14 @@ class Election:
         return self.is_leader
 
 	# TODO perform the election
-    def ballot(self,event):
+    def ballot(self, event):
+        if (self.path == None) :
+            self.path = zk.create(self.election_path + "/n_", "", ephemeral=True, sequence=True)
+        children = zk.get_children(self.election_path)
+        print "Leader expected : " + min(children)
+        self.is_leader = (self.path == self.election_path + "/" + min(children))
+        print "Elected ? " + str(self.is_leader) + " (id :" + self.path + ")"
+        zk.get_children(self.election_path, watch=self.ballot)
                             
 if __name__ == '__main__':
     zkhost = "127.0.0.1:2181" #default ZK host
@@ -29,7 +37,8 @@ if __name__ == '__main__':
 
     zk = KazooClient(zkhost)
     zk.start()
-    election = Election(zk,"/master",None,[])
+    election = Election(zk,"/master", Election.ballot, [])
+    election.ballot(None)
         
     while True:
         time.sleep(1)
